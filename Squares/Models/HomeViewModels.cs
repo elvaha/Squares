daa3c5f724fca2ae350484c720a626cdf57039f1
elvaha <elias.v.hansen@gmail.com>
2016-05-19 12:36:04 +0200

    add set and profile class
    Add set in account and see profile class

---
+++ Squares/Models/HomeViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Squares.Models
+{
+    public class Profile
+    {
+        public string Alias { get; set; }
+        public string Description { get; set; }
+        public List<string> ImageUrls { get; set; }
+    }
+}
